85690c3901008424a5626190cceaae049b1e1988
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2022-10-29 18:45:53 +0100

    test: refactor run_tests.sh and make output prettier

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
-# Runs tests
+
+EXECUTABLE="pandoc-confluence"
+OUTPUT_FILE="/tmp/pandoc-confluence-test-output.html"
 
 #------------------------------------------------------------------------------
-# Colors
+# Colors and pretty output
 #------------------------------------------------------------------------------
 
 RED="\033[0;31m"
@@ -27,17 +29,26 @@ failure() {
   printf "${RED}%s${NC}\n" "$1"
 }
 
+DIVIDER_LINE="--------------------------------------------------"
+DIVIDER_LINE_THICK="=================================================="
+
+#------------------------------------------------------------------------------
+# Build
+#------------------------------------------------------------------------------
+
+run_build() {
+  set -e
+  echo ""
+  echo $DIVIDER_LINE_THICK
+  echo "Building..."
+  echo ""
+  stack build
+  FILTER_PATH=$(stack exec -- whereis $EXECUTABLE | awk -F ': ' '{ print $2 }')
+}
+
 #------------------------------------------------------------------------------
 # Tests
 #------------------------------------------------------------------------------
-
-EXECUTABLE="pandoc-confluence"
-OUTPUT_FILE="/tmp/pandoc-confluence-test-output.html"
-
-set -e
-echo "Building..."
-stack build
-FILTER_PATH=$(stack exec -- whereis $EXECUTABLE | awk -F ': ' '{ print $2 }')
 
 keep_body_only() {
   tail -n +26 | head -n -2
@@ -52,41 +63,53 @@ generate_output() {
     keep_body_only
 }
 
-echo ""
-echo "Running tests..."
-echo ""
-
-passed=0
-failed=0
-
-for input_file in ./tests/*.md; do
-  _basename="${input_file##*/}"
-  test_name="${input_file%.*}"
-
-  generate_output "$input_file" >$OUTPUT_FILE
-  expected_output_file="${test_name}.html"
-
-  info "TESTING: $test_name"
-
-  if cmp --silent "$OUTPUT_FILE" "$expected_output_file"; then
-    success "PASSED"
-    passed=$((passed + 1))
-  else
-    failure "FAILED"
-    warn "Expected:"
-    cat "$expected_output_file"
-    warn "Got:"
-    cat "$OUTPUT_FILE"
-    echo ""
-    failed=$((failed + 1))
-  fi
+run_tests() {
+  set +e
+  echo $DIVIDER_LINE_THICK
+  echo "Running tests..."
   echo ""
-done
 
-if [ $failed -gt 0 ]; then
-  failure "Failed ${failed} / $((passed + failed)) tests!"
-  exit 1
-else
-  success "All ${passed} tests passed!"
-  exit 0
-fi
+  passed=0
+  failed=0
+
+  for input_file in ./tests/*.md; do
+    _basename="${input_file##*/}"
+    test_name="${input_file%.*}"
+
+    generate_output "$input_file" >$OUTPUT_FILE
+    expected_output_file="${test_name}.html"
+
+    info $DIVIDER_LINE
+    info "TESTING: $test_name"
+
+    if cmp --silent "$OUTPUT_FILE" "$expected_output_file"; then
+      success "PASSED"
+      passed=$((passed + 1))
+    else
+      failure "FAILED"
+      warn "Expected:"
+      cat "$expected_output_file"
+      warn "Got:"
+      cat "$OUTPUT_FILE"
+      echo ""
+      failed=$((failed + 1))
+    fi
+    echo ""
+  done
+
+  echo $DIVIDER_LINE_THICK
+  if [ $failed -gt 0 ]; then
+    failure "RESULT: Failed ${failed} / $((passed + failed)) tests!"
+  else
+    success "RESULT: All ${passed} tests passed!"
+  fi
+  echo $DIVIDER_LINE_THICK
+  exit $failed
+}
+
+#------------------------------------------------------------------------------
+# Main / entry point
+#------------------------------------------------------------------------------
+
+run_build
+run_tests
